d51e35b33f9db57909bf651c6fb9a85c0c80bf29
shouryakhare98 <shouryakhare98@gmail.com>
2020-03-09 16:21:11 -0400

    golf graph complete for dr2

--- js/golf.js
+++ js/golf.js
@@ -6,7 +6,7 @@ var svgWidth = +svg.attr("width");
 var svgHeight = +svg.attr("height");
 
 // Define margins and graph dimensions
-var margin = {top:40, left:30, bottom:20, right:30}
+var margin = {top:40, left:47, bottom:40, right:30}
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
@@ -24,7 +24,7 @@ tournaments.forEach(function(tournament) {
 // Create date parser
 var parseDate = d3.timeParse('%Y');
 
-// Read golf data
+// Read drive distance data
 d3.csv("./data/golf_drive_distance_yards.csv").then(function(data) {
 
     // Create scales for axes
@@ -38,18 +38,26 @@ d3.csv("./data/golf_drive_distance_yards.csv").then(function(data) {
         .range([height, 0])
         .nice();
 
-    // Draw axes
+    // Draw axes and axes lables
     var xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
     graphG.append("g")
         .attr("class", "x axis")
-        .attr("transform", "translate(30,570)")
-        .call(xAxis);
+        .attr("transform", "translate(" + [margin.left,svgHeight - margin.bottom] + ")")
+        .call(xAxis)
+        .append("text")
+        .attr("class", "x label")
+        .attr("transform", "translate(312,30)")
+        .text("Year");
 
     var yAxis = d3.axisLeft(yScale);
     graphG.append("g")
         .attr("class", "y axis")
         .attr("transform", "translate(" + [margin.left,margin.bottom] + ")")
-        .call(yAxis);
+        .call(yAxis)
+        .append("text")
+        .attr("class", "y label")
+        .attr("transform", "translate(-33,275) rotate(-90)")
+        .text("Average Drive Distance (Yards)");
 
     // Draw tournamets and add callbacks for checkbox updates
     tournaments.forEach(function(tournament) {
@@ -98,4 +106,34 @@ d3.csv("./data/golf_drive_distance_yards.csv").then(function(data) {
                 .remove();
         }
     }
+
+    // Read golf innovation data
+    d3.json("./data/golf_innovations.json").then(function(innovations) {
+
+        // Draw vertical line for each innovation
+        var innov_line = graphG.selectAll(".innovations")
+            .data(innovations)
+            .enter()
+            .append("g")
+            .attr("class", "innovations");
+
+        innov_line.append("line")
+            .attr("x1", function(d) {
+                return xScale(d.year) + margin.left;
+            })
+            .attr("y1", margin.top)
+            .attr("x2", function(d) {
+                return xScale(d.year) + margin.left;
+            })
+            .attr("y2", svgHeight - margin.bottom);
+
+        // Create innovation labels
+        innov_line.append("text")
+            .attr("class", "label")
+            .attr("x", function(d) {
+                return xScale(d.year) + margin.left;
+            })
+            .attr("y", margin.top - 5)
+            .text(function(d) { return d.name + ", " + d.year; });
+    });
 });
